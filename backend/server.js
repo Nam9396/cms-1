f7f722fe4e509d5d8cd348136fe05f6a1a47cb11
Nam9396 <nguyenthanhnam9396@gmail.com>
2023-09-21 10:12:06 +0700

    change PORT to 5000

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const upload = multer({
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const __dirname = path.resolve();
 connectDB();
